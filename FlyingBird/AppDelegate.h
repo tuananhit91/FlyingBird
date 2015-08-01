014e633c1710b6eb8fbe86d0e8489adab06f16d4
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-08-01 10:57:19 +0700

    Initial Commit

---
+++ FlyingBird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlyingBird
+//
+//  Created by admin on 8/1/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlyingBird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlyingBird
+//
+//  Created by admin on 8/1/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
