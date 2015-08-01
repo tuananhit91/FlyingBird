f7654507ea52bc300d7c6a09a880d4433caaf572
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-08-01 14:06:03 +0700

    update
    update

--- FlyingBird/ViewController.m
+++ FlyingBird/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
+{
+    UIImageView* bird;
+    AVAudioPlayer* player;
+}
 
 @end
 
@@ -16,7 +22,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    [self drawJungle];
+    [self addBird];
+    [self flyUpAndDown];
+    NSURL* urlSound = [[NSBundle mainBundle] URLForResource:@"ChaoMao" withExtension:@"mp3"];
+    [self soundFlyingBird:urlSound];
+}
+
+- (void) soundFlyingBird:(NSURL*) url{
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    player.numberOfLoops = -1;
+    [player play];
+}
+
+- (void) drawJungle{
+    UIImageView* background = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"jungle.jpg"]];
+    background.frame = self.view.bounds;
+    background.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:background];
+}
+
+- (void) addBird{
+    bird = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 110, 68)];
+    bird.animationImages = @[[UIImage imageNamed:@"bird0.png"],
+                             [UIImage imageNamed:@"bird1.png"],
+                             [UIImage imageNamed:@"bird2.png"],
+                             [UIImage imageNamed:@"bird3.png"],
+                             [UIImage imageNamed:@"bird4.png"],
+                             [UIImage imageNamed:@"bird5.png"]
+                             ];
+    bird.animationRepeatCount = -1; //forever loop
+    bird.animationDuration = 1; // khoảng thời gian chạy animation
+    [self.view addSubview:bird];
+    [bird startAnimating];
+}
+
+- (void) flyUpAndDown{
+    bird.transform = CGAffineTransformIdentity;
+    [UIView animateWithDuration:5 animations:^{
+        bird.center = CGPointMake(self.view.bounds.size.width, self.view.bounds.size.height);
+    }completion:^(BOOL finished){
+        bird.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(-1, 1),
+                                                 CGAffineTransformMakeRotation(M_PI_4));
+        [UIView animateWithDuration:5 animations:^{
+            bird.center = CGPointMake(0, 0);
+        }completion:^(BOOL finished){
+            [self flyUpAndDown];
+        }];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
